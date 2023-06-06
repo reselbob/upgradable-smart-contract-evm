801225b1afbecec3bc094afb5c89adeeedbc8972
Bob Reselman <bob@CogArtTech.com>
2023-06-06 09:23:10 -0700

    Create upgrade.sol

---
+++ upgrade.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract smartContract001 is Initializable, UUPSUpgradeable,  OwnableUpgradeable {
+    function initialize() initializer public {
+            __Ownable_init();
+            __UUPSUpgradeable_init();
+    }
+
+    function getSecretHelloMessage()  public pure returns (string memory){
+        return "Hello World";
+    }
+ 
+    function getSecretGoodbyeMessage()  public pure returns (string memory){
+        return "Goodbye World";
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal onlyOwner override{}
+}
