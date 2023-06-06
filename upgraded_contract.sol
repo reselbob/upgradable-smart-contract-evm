0bec2e5025bd7c0b290d7ed015a32d1de3fea846
Bob Reselman <bob@CogArtTech.com>
2023-06-06 09:51:15 -0700

    Update upgraded_contract.sol

--- upgraded_contract.sol
+++ upgraded_contract.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract smartcontract is Initializable, UUPSUpgradeable,  OwnableUpgradeable {
+contract SmartContract is Initializable, UUPSUpgradeable,  OwnableUpgradeable {
     function initialize() initializer public {
             __Ownable_init();
             __UUPSUpgradeable_init();
